54e6d2a711f55712f95ac3a790bd64f3e45312d1
Shreyansh Agarwal <shreyanshagarwal.16.2022@gmail.com>
2022-12-22 14:50:29 +0530

    Added logo Component

---
+++ profolio/src/components/logo.tsx
@@ -0,0 +1,10 @@
+import React,{FC} from 'react';
+
+const Logo:FC = () =>{
+    return <div id="logo">
+    <img src='./' alt="logo" id="logoicon" />
+    <p id="logoname">ProFolio</p>
+</div>;
+}
+
+export default Logo;

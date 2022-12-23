f8b190c7d450d399c1a1b8931a6615306c473c4f
Archiehere <cheeku67@gmail.com>
2022-12-23 19:12:47 +0530

    svg import fix

--- src/components/LoginApp.tsx
+++ src/components/LoginApp.tsx
@@ -6,26 +6,28 @@ import Input from './authinput';
 import Switch from './authswitch';
 import Authblock from './authblock';
 import Heading from './heading';
-import Illustration from './illustration'
-import { useNavigate } from "react-router-dom";
+const illustration: string = require("../images/loginImage.svg").default;
+// import { useNavigate } from "react-router-dom";
 
-const Login:FC = () => {
-    const Navhandler = useNavigate()
+const Login = () => {
+    // const Navhandler = useNavigate()
 
-    const f =() => {
-        Navhandler('/SignUp')
-    }
+    // const f =() => {
+    //     Navhandler('/SignUp')
+    // }
 
     return <div>
     <Heading />  
-    <Illustration type="login" />
+    {/* <Illustration type="login" /> */}
+    
+    <img className="illustration" src={illustration} alt="" />
     <div id = "login">
       <Oauth />
       <Input lable='Email Address' placeholder='Enter Email' message='Enter Valid Email Address'/>
       <Input lable='Password' placeholder='Enter Your Password' message='Must be at least 8 characters.'/>
       <a id="forgot_password">Forgot Password?</a>
       <Authblock name="Log In" />
-      {/* <Switch status="Don't" action='Sign Up' handleToNavigate={f}> */}
+      {/* <Switch status="Don't" action='Sign Up' > */}
     </div>
     </div>
 }

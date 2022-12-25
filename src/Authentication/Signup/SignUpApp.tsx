41415a264aeaeeede65296d69649622bb37d0e4f
Archiehere <cheeku67@gmail.com>
2022-12-25 18:08:00 +0530

    signup api integrated

--- src/Authentication/Signup/SignUpApp.tsx
+++ src/Authentication/Signup/SignUpApp.tsx
@@ -1,18 +1,34 @@
-import React from "react"
+import React ,{useState} from "react"
 import './signup.css' ;
 import Oauth from '../components/loginbox';
 import Input from '../components/authinput';
 import Switch from '../components/authswitch';
 import Authblock from '../components/authblock';
 import Heading from '../components/heading';
+import axios from "axios";
 const illustration: string = require("../images/signup.svg").default;
 
 const SignUp = () => {
-
-    function handlepass() {
-        console.log(1)
+  
+  const [email,setemail] = useState("");  
+  function handlemail(e:any) {
+        setemail(e.target.value);
       }
 
+  function handleapi(){
+    axios.post("https://linkedin-back.azurewebsites.net/auth/otp/email/send/",{
+     email:email,
+     context:"register" 
+    }).then((res) => {
+      console.log(res.data);
+      // localStorage.setItem("accesstoken" , res.data.tokens.access);
+    })
+      .catch((err) => {
+        console.log(err);
+      }
+      );
+  }
+  
     return <div>
     <Heading />  
     <img className="illustration"src={illustration} alt="" />
@@ -20,8 +36,8 @@ const SignUp = () => {
     <img className="illustration" src={illustration} alt="Enter phone number" />
     <div id = "signup">
       <Oauth status="Sign up" />
-      <Input onchange={handlepass} type="text" lable='Email Address' placeholder='Enter Email' message='Enter Valid Email Address'/>
-      <Authblock name="Sign Up" />
+      <Input onchange={handlemail} type="text" lable='Email Address' placeholder='Enter Email' message='Enter Valid Email Address'/>
+      <Authblock onclick={handleapi}name="Sign Up" />
       <Switch status="Already" action='Log In' />
     </div>
     </div>

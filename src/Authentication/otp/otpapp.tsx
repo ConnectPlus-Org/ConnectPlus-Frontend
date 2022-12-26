9c52209d756a1bf75522fccd61613302c4c3990e
Shreyansh Agarwal <shreyanshagarwal.16.2022@gmail.com>
2022-12-26 10:03:33 +0530

    Bug Fixed: Resend Timer reset

--- src/Authentication/otp/otpapp.tsx
+++ src/Authentication/otp/otpapp.tsx
@@ -36,7 +36,7 @@ const Otp = () => {
         <p className="topline">Check For OTP</p>
         <p className="middle">Enter the OTP sent to your email address </p>
         <Otpbox />
-        <p className="lowline">Didn't get OTP?<span id ="resend"> Resend OTP </span> 0:{seconds}</p>
+        <p className="lowline">Didn't get OTP?<span id ="resend" onClick={() => setSeconds(60)}> Resend OTP </span> 0:{seconds}</p>
     
       <Authblock name="Verify" />
     </div>

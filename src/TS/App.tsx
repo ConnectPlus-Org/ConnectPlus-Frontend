adddbd62198187ab52e85235432257331971c1de
Archiehere <cheeku67@gmail.com>
2022-12-28 02:04:33 +0530

    phone otp page

--- src/TS/App.tsx
+++ src/TS/App.tsx
@@ -4,6 +4,7 @@ import Login from "../Authentication/login/LoginApp";
 import SignUp from "../Authentication/Signup/SignUpApp";
 import Phone from "../Authentication/Signup/signup";
 import OTP from "../Authentication/otp/otpapp"
+import PhoneOTP from "../Authentication/phoneotp/phoneotpapp"
 import Forgot from "../Authentication/forgot/forgotAPP";
 import Resetpass from "../Authentication/reset password/resetPassApp"
 import Setpass from "../Authentication/set password/setPassApp"
@@ -15,6 +16,7 @@ const App = () => {
     <Route path="/signup" element={<SignUp />}></Route>
     <Route path="/authphone" element={<Phone />}></Route>
     <Route path="/otp" element={<OTP />}></Route>
+    <Route path="/phoneotp" element={<PhoneOTP />}></Route>
     <Route path="/forgot_password" element={<Forgot />}></Route>
     <Route path="/reset_password" element={<Resetpass />}></Route>
     <Route path="/set_password" element={<Setpass />}></Route>

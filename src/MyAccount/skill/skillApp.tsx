19def2ca52fa1a191aa0e2000da836f817107ddc
Shreyansh Agarwal <shreyanshagarwal.16.2022@gmail.com>
2023-01-19 22:28:46 +0530

    added skill search response list

--- src/MyAccount/skill/skillApp.tsx
+++ src/MyAccount/skill/skillApp.tsx
@@ -1,8 +1,9 @@
 import axios from "axios";
 import { config } from "process";
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router";
 import Nav from "../../navbar/navbar";
+import Skillcomponent from "./searchbox";
 import "./skill.css"
 
 const Skill = () => {
@@ -12,7 +13,6 @@ const Skill = () => {
     borderLeft:'3px solid #A950FB',
    
 }
-
 var accesstoken=localStorage.getItem("accesstoken");
 
 const config ={
@@ -21,19 +21,22 @@ const config ={
   }
 }
 
+var [searches,setsearches] = useState([])
+var [skill,setskill] = useState("")
+
+var l:number = searches.length
 function handleskill (e:any){
-  const value:string = e.target.value
-  console.log(value)
-  axios.get('https://linkedin-backend.azurewebsites.net/profile/skill/list/?search_input='+value,config)
+  setskill(e.target.value)
+  axios.get('https://linkedin-backend.azurewebsites.net/profile/skill/list/?search_input='+e.target.value,config)
   .then((res)=>
   {
     console.log(res.data);
+    setsearches(res.data);
   })
   .catch((err)=>{
     console.log(err);
   })
 }
-
   return (
     <div>
       <Nav />
@@ -50,7 +53,14 @@ function handleskill (e:any){
         <div>
           Skill
           <br />
-          <input placeholder="Enter Skill" onChange={handleskill}/>
+          <input placeholder="Enter Skill" onChange={handleskill} value={skill}/>
+          <div style={{height:"15vw"}}>
+          <div className="dropsearchbox" onClick={()=>{setsearches([]); const skillname:string=sessionStorage.getItem("skillname")!; setskill(skillname)}}>
+          {
+            searches.map((box:any)=>{return <Skillcomponent key={box.id} box={box} />})
+          }
+          </div>
+          </div>
         </div>
     <button>Save</button>
       </div>

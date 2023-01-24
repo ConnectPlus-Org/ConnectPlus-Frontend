1543d00dab185ff5dcc1ae49d5e5bc84b0de5824
Archiehere <cheeku67@gmail.com>
2023-01-24 12:29:46 +0530

    addded pagination and loader

--- src/MyAccount/Main/components/test.tsx
+++ src/MyAccount/Main/components/test.tsx
@@ -7,13 +7,13 @@ const viewusername = sessionStorage.getItem("viewusername")
 const TestBox:any = (box:any) => {
     if(box.box.end_date == null)
     return <div className="expbox">
-        <div>
+        {/* <div>
         <p style={{fontWeight: '700',fontSize: '1.7vw'}}>{box.box.organization_data.name}</p>
         <p style={{fontWeight: '700',fontSize: '1.2vw',color:"#E6E6EB"}}>{box.box.title}</p>
         <p style={{fontWeight: '400',fontSize: '1.2vw',color:"#EBEBF0"}}>Test date:{box.box.test_date}</p>
         <p style={{fontWeight: '400',fontSize: '1vw',color:"#EBEBF0"}}>Score: {box.box.score} </p>
         </div>
-        {(username===viewusername)?<img  className="editicon action" src={edit} />:null}
+        {(username===viewusername)?<img  className="editicon action" src={edit} />:null} */}
     </div>
 }
 

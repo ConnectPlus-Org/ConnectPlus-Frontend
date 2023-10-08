9a6efe5923ebce0bda1a174e8e8bb40323ec26b7
Tech-Shreyansh <shreyanshagarwal.16.2022@gmail.com>
2023-10-08 14:35:50 +0530

    updated backend url

--- src/BaseUrl.tsx
+++ src/BaseUrl.tsx
@@ -3,5 +3,5 @@ import axios from "axios";
 
 export default axios.create({
     // baseURL:"http://linkedin-backend.centralindia.cloudapp.azure.com/"
-    baseURL:"http://13.233.90.200/"
+    baseURL:"https://connectplus.scriptclub.tech/"
 })
